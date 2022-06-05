837a5a16b4f8842d0b4992d47864b0257a0bf338
Madhusha <madhushadavidmathivannan@gmail.com>
2022-06-06 01:55:04 +0530

    Commit Changes

--- src/app/service/vision.service.ts
+++ src/app/service/vision.service.ts
@@ -35,29 +35,29 @@ export class VisionService {
       img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
     },
     {
-      title: 'Card Title 1',
+      title: 'Card Title 5',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
       buttonText: 'Button',
       img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
     },
-    {
-      title: 'Card Title 2',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-    {
-      title: 'Card Title 3',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-    {
-      title: 'Card Title 4',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
+    // {
+    //   title: 'Card Title 2',
+    //   description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+    //   buttonText: 'Button',
+    //   img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    // },
+    // {
+    //   title: 'Card Title 3',
+    //   description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+    //   buttonText: 'Button',
+    //   img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    // },
+    // {
+    //   title: 'Card Title 4',
+    //   description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+    //   buttonText: 'Button',
+    //   img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    // },
 
   ];
 
